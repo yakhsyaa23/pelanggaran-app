f6697df70059acfe411a0ddd96fd0b48b8e4b531
damayantievi400-ui <damayantievi400@gmail.com>
2026-01-19 07:59:09 +0800

    Tampilan login dan menghilangkan register

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,17 @@ use App\Http\Controllers\ViolationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.index');
-})->name('home');
+    return redirect()->route('home');
+});
 
-Route::get('violations/search-student', [ViolationController::class, 'searchStudent'])->name('violations.searchStudent');
-Route::resource('violations', ViolationController::class);
+
+Route::group(['middleware' => ['auth']], function () {
+    //
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('violations/search-student', [ViolationController::class, 'searchStudent'])->name('violations.searchStudent');
+    Route::resource('violations', ViolationController::class);
+});
+
+
+Auth::routes(['register' => false, 'reset' => false]);
+
